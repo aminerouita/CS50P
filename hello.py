f24b99b4400c078bfb0966dcebb4132311a19201
AMINE ROUITA <amine.rouita@gmail.com>
2025-06-17 23:57:07 +0100

    Finished hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 # This is a simple Python script that greets the user.
 ## Ask user for their name
-name = input("What's your name? ")
+name = input("What's your full name? ")
+
+#remove whitespace from str and capitalize user's name
+name= name.strip().title()
+
+#Split user's name into first and last name
+fname, lname = name.split()
+
 ## Greet user
-print("hello, " + name + "!")
+print(f"hello, {fname} !")
+

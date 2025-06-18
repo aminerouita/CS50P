1bb3e56d1c66f33840280297dbddb551f3fbf0f4
AMINE ROUITA <amine.rouita@gmail.com>
2025-06-18 03:49:17 +0100

    finished and created w0 directory

--- calculator.py
+++
@@ -1,7 +0,0 @@
-# This script takes two inputs from the user and prints their sum.
-
-x = int(input("Enter the value for x: "))
-y = int(input("Enter the value for y: "))
-
-
-print("The sum of x and y is:", x+y)
